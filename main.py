868d8a4c8fa0474d2d91cc2375de4a1c1e4f8913
Manu <mcorcos@itba.edu.ar>
2022-05-31 11:32:43 -0300

    avanzo con la integracion de los widgets con el codigo

--- main.py
+++ main.py
@@ -8,23 +8,44 @@ class iniciar:
         self.ventana.show()
 
         self.ventana.actionSalir_2.triggered.connect(self.click_actionsalir)
-        self.ventana.actionDatos.triggered.connect(self.click_actionDatos)
+    #    self.ventana.actionDatos.triggered.connect(self.click_actionDatos)
+        self.ventana.graphicButton.clicked.connect(self.graphics)
         app.exec()
 
         #sis.exit
     def click_actionsalir(self):
         sys.exit()
     
-    def click_actionDatos(self):
+  #  def click_actionDatos(self):
         self.x01 = uic.loadUi("ventana_x01.ui")
         self.x01.show()
         self.x01.pb_cancelar.clicked.connect(self.clickcancelar)
 
-    def clickcancelar(self):
+  #  def clickcancelar(self):
         self.x01.close()
+    
+    def graphics(self):
 
+        if(self.ventana.sineButton.isChecked()):
+            Wo = self.ventana.inputWo.text()
+            Fz = self.ventana.inputFz.text()
+            Amp = self.ventana.inputAmp.text()
+            K = self.ventana.inputK.text()
+            plotSignal()
+            
 
+        elif(self.ventana.sawButton.isChecked()):
+            plotSignal()
+            return
+        elif(self.ventana.triangularButton.isChecked()):
+            plotSignal()
+            return
+        elif(self.ventana.pulseButton.isChecked()):
+            plotSignal()
+            return   
 
+def plotSignal():
+    return
 
 iniciar()
 
